d86829739b061c2727a4ad712ccafbba21187699
vmancier <sniper33470@hotmail.fr>
2015-03-16 15:25:25 +0100

    création classe Game_Model et Game_View
    Signed-off-by: vmancier

---
+++ SpaceKill/Game_Model.hpp
@@ -0,0 +1,11 @@
+#ifndef GAME_MODEL_HPP_INCLUDED
+#define GAME_MODEL_HPP_INCLUDED
+
+class Game_Model
+{
+private:
+public:
+
+};
+
+#endif // GAME_MODEL_HPP_INCLUDED
---
+++ SpaceKill/Game_View.hpp
@@ -0,0 +1,11 @@
+#ifndef GAME_VIEW_HPP_INCLUDED
+#define GAME_VIEW_HPP_INCLUDED
+
+class Game_View
+{
+private:
+public:
+
+};
+
+#endif // GAME_VIEW_HPP_INCLUDED
